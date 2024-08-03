eec4ded87c2850f1e57f79de6c07a72390e18b57
Khaled Radwan <khaledradwan96@gmail.com>
2024-08-03 09:52:48 +0300

    Create website Compounds

---
+++ Template/TemplateName.jsx
@@ -0,0 +1,15 @@
+// TemplateName.jsx
+/* eslint-disable no-unused-vars */
+
+import React , { useState , useEffect} from 'react'
+import style from './TemplateName.module.css'
+
+export default function TemplateName() {
+    const [Count, setCount] = useState(0)
+    useEffect(()=>{
+
+    }, [])
+    return <>
+        <h2>TemplateName</h2>
+    </>
+}
