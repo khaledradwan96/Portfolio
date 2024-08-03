1a57d9ae10b375610ca9cd9dd9bcafc6a3a2f716
Khaled Radwan <khaledradwan96@gmail.com>
2024-08-03 22:15:11 +0300

    create and code NavBar

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -13,7 +13,6 @@ export default function Layout() {
 
     }, [])
     return <>
-        <h2>Layout</h2>
         <NavBar/>
         <div className='container mx-auto p-2'>
             <Outlet/>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,14 +2,38 @@
 /* eslint-disable no-unused-vars */
 
 import React , { useState , useEffect} from 'react'
-import style from './NavBar.module.css'
+import './NavBar.css'
+import { NavLink } from 'react-router-dom'
+import About from '../About/About'
 
 export default function NavBar() {
     const [Count, setCount] = useState(0)
     useEffect(()=>{
 
     }, [])
+
+    function showNavLinks(){
+        let navLinks = document.querySelector('.navBar .navLinks')
+        navLinks.classList.toggle('hidden')
+    }
     return <>
-        <h2>NavBar</h2>
+        <nav className='navBar md:fixed top-0 left-0 right-0'>
+            <div className='container p-4 mx-auto flex flex-row flex-wrap justify-between items-center'>
+                <div className='logo'>
+                    <h3><NavLink to=''>Khaled Radwan</NavLink></h3>
+                </div>
+                <span className='md:hidden' onClick={showNavLinks}>
+                    <i className='fa-solid fa-bars'></i>
+                </span>
+                <div className='navLinks hidden md:block w-full md:w-72'>
+                    <ul className='md:flex flex-col md:flex-row'>
+                        <li><NavLink to='about'>About</NavLink></li>
+                        <li><NavLink to='portfolio'>Portfolio</NavLink></li>
+                        <li><NavLink to='contact'>Contact</NavLink></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
     </>
 }
