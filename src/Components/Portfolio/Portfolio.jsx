411d374bcd0ed03bcce9753748ec164ade8f77c5
Khaled Radwan <khaledradwan96@gmail.com>
2024-08-04 18:05:06 +0300

    add code of full screen img when click on Portfolio compound

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,14 +1,20 @@
-/* eslint-disable no-unused-vars */
 // Portfolio.jsx
 
 import './Portfolio.css'
 import poert1 from '../../assets/poert1.png'
 import poert2 from '../../assets/port2.png'
 import poert3 from '../../assets/port3.png'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export default function Portfolio() {
-    let [cardImg, setCardImg] = useState(0)
+    let [fullScreenImg, setFullScreenImg] = useState(null)
+    let handleImageClick  = (imgSrc) => {
+        setFullScreenImg(imgSrc);
+        // console.log(imgSrc)
+    }
+    let handleClose = () => {
+        setFullScreenImg(null)
+    }
     return <>
         <div className='portfolio md:pt-20 h-screen'>
             <div className="container p-4 mx-auto flex justify-center items-center">
@@ -20,44 +26,48 @@ export default function Portfolio() {
                         <span></span>
                     </div>
                     <div className='cards-group mt-5 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                        <div className='card'>
+                        <div className='card' onClick={()=> handleImageClick(poert1)}>
                             <img src={poert1} alt="" />
                             <div className='layer'>
                                 <i className="text-white fa-solid fa-plus fa-6x"></i>
                             </div>
                         </div>
-                        <div className='card'>
+                        <div className='card' onClick={()=> handleImageClick(poert2)}>
                             <img src={poert2} alt="" />
                             <div className='layer'>
                                 <i className="text-white fa-solid fa-plus fa-6x"></i>
                             </div>
                         </div>
-                        <div className='card'>
+                        <div className='card' onClick={()=> handleImageClick(poert3)}>
                             <img src={poert3} alt="" />
                             <div className='layer'>
                                 <i className="text-white fa-solid fa-plus fa-6x"></i>
                             </div>
                         </div>
-                        <div className='card'>
+                        <div className='card' onClick={()=> handleImageClick(poert1)}>
                             <img src={poert1} alt="" />
                             <div className='layer'>
                                 <i className="text-white fa-solid fa-plus fa-6x"></i>
                             </div>
                         </div>
-                        <div className='card'>
+                        <div className='card' onClick={()=> handleImageClick(poert2)}>
                             <img src={poert2} alt="" />
                             <div className='layer'>
                                 <i className="text-white fa-solid fa-plus fa-6x"></i>
                             </div>
                         </div>
-                        <div className='card'>
+                        <div className='card' onClick={()=> handleImageClick(poert3)}>
                             <img src={poert3} alt="" />
                             <div className='layer'>
                                 <i className="text-white fa-solid fa-plus fa-6x"></i>
                             </div>
                         </div>
-
                     </div>
+                    {fullScreenImg && (
+                    <div className='fullscreen-modal' onClick={handleClose}>
+                        <img src={fullScreenImg} alt="Full Screen" className='fullscreen-image' />
+                    </div>
+                )}
                 </div>
             </div>
         </div>
